197f33955d537e83a56e9d27c40e4c33f84bc219
kizzzeee0-cmyk <kizzzeee0@gmail.com>
2026-09-26 00:05:45 +0900

    Update worker.js

--- worker/worker.js
+++ worker/worker.js
@@ -28,8 +28,9 @@ function cors(r, req, env) {
   const h = new Headers(r.headers);
   const origin = req.headers.get("Origin") || "";
 
-  const allowed = origin === "https://artmug-portfolio.pages.dev"
-    || /^https:\/\/[a-z0-9-]+\.artmug-portfolio\.pages\.dev$/i.test(origin);
+  const allowed =
+    origin === "https://artmug-portfolio.pages.dev" ||
+    /^https:\/\/[a-z0-9-]+\.artmug-portfolio\.pages\.dev$/i.test(origin);
 
   if (allowed) {
     h.set("Access-Control-Allow-Origin", origin);
